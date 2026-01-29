40cb93ad5e4f4ce26e2f0922f6a4bbbf834331ce
Andrei <amborn02@gmail.com>
2026-01-29 15:36:34 -0600

    Init

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script lang="ts">
+import { inject, provide, reactive, type InjectionKey } from 'vue';
+import HelloWorld from './components/HelloWorld.vue'
+import type { Profile } from './types/type';
+import ExperienceCard from './components/ExperienceCard.vue';
+
+export default {
+  setup() {
+    const name: Profile = reactive({name: "Andrei"})
+
+    provide("name", name)
+    return {name}
+  },
+  components: {
+    ExperienceCard
+  }
+}
+
+</script>
+
+
+<template>
+  <div className="flex flex-row">
+    <div className="w-100 h-20 bg-slate-800">
+      {{ name.name }}
+    </div>
+    <ExperienceCard />
+  </div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/ExperienceCard.vue
@@ -0,0 +1,26 @@
+<script lang="ts">
+import { inject, ref } from 'vue';
+import type { Profile } from '../types/type';
+
+export default {
+    setup() {
+        const key = inject<Profile>("name")
+        const bullets = ref(['Used x to accomplish y by doing z'])
+        return { bullets, key }
+    }
+}
+
+</script>
+
+<template>
+    <div className="flex flex-col">
+        <input type="text" v-model="key!.name"/>
+        <label for="position">Position</label>
+        <input id="position" type='text'>
+        <label for="company">Company</label>
+        <input id="company" type='text'>
+        <p>Bullet Point</p>
+        <input v-for="value in bullets" v-bind:value="value" type="text" />
+        <button @click="bullets.push('Used x to accomplish y by doing z')"></button>
+    </div>
+</template>
